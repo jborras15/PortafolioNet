4d0815fa39f7a7dee604d0b400e14092570b6c66
jorge <jborras15@gmail.com>
2022-08-26 11:01:04 -0500

    se borro lo echo en el comit pasado ya que son muestra .
    se borro el codigo de la lista de proyectos de la _SeccionProyectos.cshtml para crea una vista parcial de la lista para compartila  con una vista nueva llamada Proyectos.
    
    se agrego una nueva vista Proyectos.cshtml en la cual tenemos una vista parcial de la lista de proyectos.
    
    _Layout.cshtml se agrego la ruta de proyectos

--- PortafolioNETMVC/Controllers/HomeController.cs
+++ PortafolioNETMVC/Controllers/HomeController.cs
@@ -8,38 +8,29 @@ namespace PortafolioNETMVC.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
     private readonly IRepositorioProyecto _repositorioProyecto;
-    private readonly IConfiguration _configuration;
+    
 
-    public HomeController(ILogger<HomeController> logger,
-        IRepositorioProyecto repositorioProyecto,
-        IConfiguration configuration)
+    public HomeController(
+        IRepositorioProyecto repositorioProyecto)
     {
-        _logger = logger;
         _repositorioProyecto = repositorioProyecto;
-        _configuration = configuration;
+        
     }
 
     public IActionResult Index()
     {
-        var apellido = _configuration.GetValue<string>("Apellido");
-        _logger.LogInformation("Este es un mensaje de log");
-        _logger.LogCritical("Este es un mensaje de log");
-        _logger.LogError("Este es un mensaje de log");
-        _logger.LogWarning("Este es un mensaje de log " + apellido);
-        _logger.LogDebug("Este es un mensaje de log");
-        _logger.LogTrace("Este es un mensaje de log " );
-
         var proyectos = _repositorioProyecto.ObtenerProyectos().Take(3).ToList();
         var modelo = new HomeIndexViewModel() { Proyectos = proyectos};
         return View(modelo);
     }
 
-    public IActionResult Privacy()
+    public IActionResult Proyectos()
     {
-        return View();
+        var proyectos = _repositorioProyecto.ObtenerProyectos();
+        return View(proyectos);
     }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
